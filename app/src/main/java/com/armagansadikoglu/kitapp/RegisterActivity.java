5bb5eeec2540b192fe4fc9833a09ed32fd0725e4
armagan <armagansadikoglu@gmail.com>
2020-05-06 00:06:07 +0300

    Default Profile Pic -Kullanıcılara kayıt olurken default olarak bir profil fotoğrafı atanıyor. Böylece resim yüklenmesiyle ilgili hatalar çözülmüş oluyor.

--- app/src/main/java/com/armagansadikoglu/kitapp/RegisterActivity.java
+++ app/src/main/java/com/armagansadikoglu/kitapp/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -16,11 +17,15 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -87,9 +92,26 @@ public class RegisterActivity extends AppCompatActivity {
                             displayName = FirebaseAuth.getInstance().getCurrentUser().getEmail();
                         }
                         User user = new User(FirebaseAuth.getInstance().getCurrentUser().getUid(), displayName);
-
-
                         usersDatabaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user);
+
+                        FirebaseStorage storage = FirebaseStorage.getInstance();
+                        StorageReference storageRef = storage.getReference();
+                        StorageReference profileImagesRef = storageRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/drawable/defaultpic");
+                        UploadTask uploadTask = profileImagesRef.putFile(uri);
+                        uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                            }
+                        });
+
                         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                         startActivity(intent);
                     } else {
